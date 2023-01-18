3bc0c799ebdc33f8c7b4ca54eebccb919f3eae30
Ferdinand Malcher <ferdinand@malcher.media>
2023-01-18 15:10:21 +0100

    Module Refactoring

---
+++ book-rating/src/app/books/book/book.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss']
+})
+export class BookComponent implements OnInit {
+  @Input() book?: Book;
+
+  constructor() {
+    console.log('CTOR', this.book);
+  }
+
+  ngOnInit() {
+    console.log('NGONINIT', this.book);
+
+  }
+}
