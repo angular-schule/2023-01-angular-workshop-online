70c52ceff4d180ed198a33d3b72686b7c8a69d5d
Ferdinand Malcher <ferdinand@malcher.media>
2023-01-18 11:19:47 +0100

    Styles einbinden

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -6,36 +6,3 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-
-////////////////////////////////
-
-
-class Customer {
-
-  constructor(public id: number) {}
-
-  /*id: number;
-
-  constructor(id: number) {
-    this.id = id;
-  }*/
-
-  fooBar(foo: string): number {
-
-    setTimeout(() => {
-      console.log('ID', this.id);
-    }, 2000);
-
-    return 4;
-  }
-}
-
-const myCustomer = new Customer(4);
-console.log(myCustomer);
-
-const foo = function (arg: number): number {
-  return arg + 1;
-}
-
-const foo2 = arg => arg + 1;
-
