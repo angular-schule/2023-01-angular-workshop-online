29d791d7eb639ad6b56a7f3262b354883e86a20b
Ferdinand Malcher <ferdinand@malcher.media>
2023-01-20 10:45:46 +0100

    Test Dashboard mit Spy

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,28 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 import { DashboardComponent } from './dashboard.component';
+
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
-
   beforeEach(async () => {
+    const ratingMock: BookRatingService = {
+      rateUp: (b: Book) => b,
+      rateDown: (b: Book) => b,
+    };
+
     await TestBed.configureTestingModule({
       declarations: [ DashboardComponent ],
+      schemas: [NO_ERRORS_SCHEMA], // Shallow Component Test
+      providers: [
+        // BRS ersetzen: Immer wenn jemand den Service anfordert,
+        // wird stattdessen der RatingMock ausgeliefert.
+        { provide: BookRatingService, useValue: ratingMock },
+      ]
     })
     .compileComponents();
 
@@ -25,5 +39,18 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call service.rateUp on doRateUp', () => {
+    const book = { isbn: '123' } as Book; // Type Assertion (Vorsicht!)
+
+    const service = TestBed.inject(BookRatingService);
+    // spyOn(service, 'rateUp').and.returnValue(book);
+    // spyOn(service, 'rateUp').and.callFake(b => b);
+    spyOn(service, 'rateUp').and.callThrough(); // the spy will still track all calls to it but in addition it will delegate to the actual implementation.
+
+    component.doRateUp(book);
+
+    expect(service.rateUp).toHaveBeenCalledOnceWith(book);
   });
 });
