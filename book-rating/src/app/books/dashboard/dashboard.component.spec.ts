d67f7233bc9b6c6f19fc66db3f19a7af6993eee6
Ferdinand Malcher <ferdinand@malcher.media>
2023-01-20 12:20:44 +0100

    HTTP

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 
@@ -14,6 +15,11 @@ describe('DashboardComponent', () => {
       rateUp: (b: Book) => b,
       rateDown: (b: Book) => b,
     };
+
+    // Ausblick: Ersatz für BookStoreService
+    const storeMock = {
+      getAll: () => of([])
+    }
 
     await TestBed.configureTestingModule({
       declarations: [ DashboardComponent ],
