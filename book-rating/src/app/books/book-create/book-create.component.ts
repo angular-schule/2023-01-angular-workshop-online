b068f7ea36fb02d50b3d6876f316c1eb75f5d7d6
Ferdinand Malcher <ferdinand@malcher.media>
2023-01-23 12:30:50 +0100

    Reactive Forms bis Validierung

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'br-book-create',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-create.component.scss']
 })
 export class BookCreateComponent {
-
+  bookForm = new FormGroup({
+    isbn: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+        Validators.pattern(/^[0-9]*$/)
+      ]
+    }),
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.maxLength(80)
+      ]
+    }),
+    description: new FormControl('', { nonNullable: true }),
+    rating: new FormControl(1, {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(5)
+      ]
+    }),
+    price: new FormControl(0, {
+      nonNullable: true,
+      validators: Validators.min(0)
+    }),
+  });
 }
+
+/*
+TODO
+- Fehler anzeigen
+  - allgemein ("ISBN ist ungültig")
+  - konkret ("ISBN ist zu kurz")
+- Submit-Button
+- abschicken
+- HTTP
+- bei Erfolg:
+  - navigieren (Dashboard oder Detailseite)
+  - Meldung anzeigen
+  - zurücksetzen
+
+*/
