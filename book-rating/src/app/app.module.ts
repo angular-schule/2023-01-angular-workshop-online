0bcad1a17fa4467e8dca2cdb7d01cde4ea595923
Ferdinand Malcher <ferdinand@malcher.media>
2023-01-25 11:53:14 +0100

    NgRx einrichten
    ng add @ngrx/store
    ng add @ngrx/store-devtools
    ng add @ngrx/effects
    ng add @ngrx/schematics

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 // import { BooksModule } from './books/books.module';
 
 @NgModule({
@@ -14,7 +17,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     // BooksModule,
-    HttpClientModule, // provideHttpClient() für Standalone Components
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([]), // provideHttpClient() für Standalone Components
     // RouterModule.forChild([{ path: '**', redirectTo: '/books' }]) // Wildcard-Route. Muss ganz guten stehen!
   ],
   providers: [],
